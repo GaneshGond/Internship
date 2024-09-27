1bcf0c37ae4679ad0ff35991bc91add0bdeecfe4
GaneshGond <136343354+GaneshGond@users.noreply.github.com>
2024-09-27 22:30:56 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -19,11 +19,11 @@ function Home(){
         <h1 className="title">GANESH</h1>
         <p className="section__text__p2">Frontend Developer</p>
         <div className="btn-container">
-          <button href="https://drive.google.com/file/d/1lp0-WWDSRsZ5AiQhOiSqyrfcmTXLSqc0/view?usp=drivesdk"
+         <a> <button href="https://drive.google.com/file/d/1lp0-WWDSRsZ5AiQhOiSqyrfcmTXLSqc0/view?usp=drivesdk"
             className=" btn-color-2"
           >
             Download CV
-          </button>
+          </button></a>
           <button className=" btn-color-1">
             Contact Info
           </button>
